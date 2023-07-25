e6861c726c004fa8b4d80e0d52fb7718da301115
Kulikov Alexander <shouter84@yandex.ru>
2023-07-25 23:17:46 +0300

    Added solution for task_22

--- task_22.py
+++ task_22.py
@@ -7,3 +7,30 @@
 # множества. m — кол-во элементов второго множества. Затем
 # пользователь вводит сами элементы множеств.
 
+from random import randint
+
+n = int(input('Enter size of list_n: '))
+m = int(input('Enter size of list_m: '))
+
+list_n = [randint(-5, 20) for i in range(n)]
+list_m = [randint(-5, 20) for i in range(m)]
+print(list_n)
+print(list_m)
+
+list_n = set(list_n)
+list_m = set(list_m)
+print(list_n)
+print(list_m)
+
+list_1 = list_n.intersection(list_m)
+
+list_2 = list(list_1)
+print(list_2)
+
+for i in range(len(list_2) - 1):
+    for j in range(len(list_2) - i - 1):
+        if list_2[j] > list_2[j + 1]:
+            (list_2[j], list_2[j + 1]) = (list_2[j + 1], list_2[j]) 
+print(list_2)
+
+
